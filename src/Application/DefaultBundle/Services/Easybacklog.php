b9e724ee9d7593f6dfbe5a8e4dfe9d4799499814
Mike Pearce <mike@mikepearce.net>
2013-04-26 17:48:32 +0100

    Added extra params

--- src/Application/DefaultBundle/Services/Easybacklog.php
+++ src/Application/DefaultBundle/Services/Easybacklog.php
@@ -4,12 +4,25 @@ namespace Application\DefaultBundle\Services;
 
 class Easybacklog {
 
-	public function __construct($guzzle) {
-		var_dump($guzzle);
+    // HTTP Service
+    private $guzzle;
+
+    /**
+     * Where are we going to store it?
+     **/
+    private $json_file = 'stories.json';
+
+    /**
+     * 
+     **/
+	public function __construct($guzzle, $api_key, $accountid, $backlogs) {
+        var_dump($backlogs);
+        // Set it
+		$this->guzzle = $guzzle;
 	}
 
-	public function getStuff() {
-		return 'stuff';
+	public function getEndpoint() {
+		return $this->guzzle->getBaseUrl();
 	}
 	
 }
