aec62a9554b269928e837ddaead1f1369004699c
Mike Pearce <mike@mikepearce.net>
2013-04-26 14:37:53 +0100

    Made guzzle non-optional dependency

--- src/Application/DefaultBundle/Services/Easybacklog.php
+++ src/Application/DefaultBundle/Services/Easybacklog.php
@@ -4,19 +4,12 @@ namespace Application\DefaultBundle\Services;
 
 class Easybacklog {
 
-	public function __construct() {
-		//var_dump($guzzle);
+	public function __construct($guzzle) {
+		var_dump($guzzle);
 	}
 
 	public function getStuff() {
 		return 'stuff';
 	}
-
-	/**
-     * Injecting guzzle is optional
-     **/
-    public function setGuzzle($guzzle = null) {
-        var_dump($guzzle);
-    }
 	
 }
