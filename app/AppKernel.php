fdb82a17769d99fe52b16a19f5085af1bab23263
Mike Pearce <mike@mikepearce.net>
2013-04-26 18:08:23 +0100

    Added the EasybacklogAPI bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Application\DefaultBundle\ApplicationDefaultBundle(),
+            new Mikepearce\EasybacklogApiBundle\MikepearceEasybacklogApiBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Mikepearce/EasybacklogApiBundle/MikepearceEasybacklogApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Mikepearce\EasybacklogApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MikepearceEasybacklogApiBundle extends Bundle
+{
+}
