1427e43a8e4f897c3428038170c031f20d551b5d
Mike Pearce <mike@mikepearce.net>
2013-04-28 22:54:39 +0100

    Removed teh pointless trendsetter bundle and moved functionaity into lib of main bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,7 +21,6 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Application\DefaultBundle\ApplicationDefaultBundle(),
             new Mikepearce\EasybacklogApiBundle\MikepearceEasybacklogApiBundle(),
-            new Application\TrendsetterBundle\ApplicationTrendsetterBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
