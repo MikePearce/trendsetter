eea03501cdbdd209e1c53d1f07e55f6c2b567d63
Mike Pearce <mike@mikepearce.net>
2013-04-30 12:31:21 +0100

    Tidied the estimates controller a little

--- src/Application/DefaultBundle/Controller/EstimatesController.php
+++ src/Application/DefaultBundle/Controller/EstimatesController.php
@@ -52,9 +52,6 @@ class EstimatesController extends Controller
           foreach (array(1, 2, 3, 5, 8, 13, 20) AS $est) {
             $rowy[] = array('v' => (isset($estimates[$est]) ? $estimates[$est] : 0), 'f' => null);
           }
-          // foreach ($estimates AS $size => $count) {
-            
-          // }
           $row['c'] = array_merge($rowx, $rowy);
           $rows[] = $row;
           $row = $rowx = $rowy = array();
@@ -62,11 +59,8 @@ class EstimatesController extends Controller
         
       }
 
-      $return_array = array('cols' => $columns, 'rows' => $rows);
-
-      //$array =  json_decode('{"cols":[{"id":"","label":"Month","pattern":"","type":"string"},{"id":"","label":"Size: 1","pattern":"","type":"number"},{"id":"","label":"Size: 2","pattern":"","type":"number"},{"id":"","label":"Size: 3","pattern":"","type":"number"},{"id":"","label":"Size: 5","pattern":"","type":"number"},{"id":"","label":"Size: 8","pattern":"","type":"number"},{"id":"","label":"Size: 13","pattern":"","type":"number"},{"id":"","label":"Size: 20","pattern":"","type":"number"}],"rows":[{"c":[{"v":"2012/10","f":null},{"v":2,"f":null},{"v":0,"f":null},{"v":4,"f":null},{"v":6,"f":null},{"v":3,"f":null},{"v":0,"f":null},{"v":0,"f":null}]},{"c":[{"v":"2012/12","f":null},{"v":1,"f":null},{"v":0,"f":null},{"v":2,"f":null},{"v":1,"f":null},{"v":1,"f":null},{"v":0,"f":null},{"v":0,"f":null}]},{"c":[{"v":"2012/09","f":null},{"v":0,"f":null},{"v":1,"f":null},{"v":0,"f":null},{"v":1,"f":null},{"v":2,"f":null},{"v":1,"f":null},{"v":1,"f":null}]},{"c":[{"v":"2012/11","f":null},{"v":4,"f":null},{"v":5,"f":null},{"v":11,"f":null},{"v":8,"f":null},{"v":3,"f":null},{"v":0,"f":null},{"v":0,"f":null}]},{"c":[{"v":"2012/07","f":null},{"v":0,"f":null},{"v":0,"f":null},{"v":0,"f":null},{"v":1,"f":null},{"v":0,"f":null},{"v":3,"f":null},{"v":1,"f":null}]},{"c":[{"v":"2012/08","f":null},{"v":0,"f":null},{"v":0,"f":null},{"v":0,"f":null},{"v":1,"f":null},{"v":0,"f":null},{"v":0,"f":null},{"v":0,"f":null}]},{"c":[{"v":"2013/01","f":null},{"v":2,"f":null},{"v":1,"f":null},{"v":3,"f":null},{"v":7,"f":null},{"v":3,"f":null},{"v":2,"f":null},{"v":0,"f":null}]},{"c":[{"v":"2013/02","f":null},{"v":1,"f":null},{"v":6,"f":null},{"v":5,"f":null},{"v":13,"f":null},{"v":10,"f":null},{"v":2,"f":null},{"v":0,"f":null}]},{"c":[{"v":"2013/03","f":null},{"v":1,"f":null},{"v":7,"f":null},{"v":4,"f":null},{"v":14,"f":null},{"v":4,"f":null},{"v":1,"f":null},{"v":0,"f":null}]},{"c":[{"v":"2013/04","f":null},{"v":7,"f":null},{"v":5,"f":null},{"v":4,"f":null},{"v":10,"f":null},{"v":0,"f":null},{"v":0,"f":null},{"v":0,"f":null}]}],"p":null}', true);
       $response = new JsonResponse();
-      $response->setData($return_array);
+      $response->setData(array('cols' => $columns, 'rows' => $rows));
       $response->headers->set('Content-Type', 'application/json');
       return $response;
     }
