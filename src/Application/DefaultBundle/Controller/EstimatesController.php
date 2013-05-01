5fa02882e57e26d06058143e69c930dcd3fa4ba1
Mike Pearce <mike@mikepearce.net>
2013-05-01 10:01:27 +0100

    Added another chart for total stories

--- src/Application/DefaultBundle/Controller/EstimatesController.php
+++ src/Application/DefaultBundle/Controller/EstimatesController.php
@@ -17,52 +17,30 @@ class EstimatesController extends Controller
         );
     }
 
-    public function dataAction() {
-      
-      // Do the columns
-      $columns = array();
-      foreach(array(
-        'Month'     => 'string', 
-        'Size: 1'   => 'number', 
-        'Size: 2'   => 'number', 
-        'Size: 3'   => 'number', 
-        'Size: 5'   => 'number', 
-        'Size: 8'   => 'number', 
-        'Size: 13'  => 'number', 
-        'Size: 20'  => 'number') AS $field => $type) {
-        $columns[] = array('id' => '', 'label' => $field, 'pattern' => '', 'type' => $type);
-      }
-
-      // Do the data
-      $rows = array();
+    public function dataAction($type = 'estimatespread') {
       
       $easybacklogClient = $this->get('mikepearce_easybacklog_api');
       $easybacklogClient->setAccountId('477')
                         ->setBacklog(array(9248,7869,9555));
-      $estimates = new Estimates();
-      $estimate_data = $estimates->getEstimateDataByMonth(
-          $easybacklogClient->getStoriesFromTheme()
-      );
-
-
-      $rows = array();
-      foreach($estimate_data AS $year => $month) {
-        foreach($month AS $month_no => $estimates) {
-          $rowx = array(array('v' => $year ."/". $month_no, 'f' => null));
-          foreach (array(1, 2, 3, 5, 8, 13, 20) AS $est) {
-            $rowy[] = array('v' => (isset($estimates[$est]) ? $estimates[$est] : 0), 'f' => null);
-          }
-          $row['c'] = array_merge($rowx, $rowy);
-          $rows[] = $row;
-          $row = $rowx = $rowy = array();
-        }
-        
+      
+      $estimates = new Estimates($easybacklogClient);
+      
+      switch($type) {
+        case 'estimatespread':
+          $data = $estimates->getEstimateSpreadPerMonth();
+          break;
+        case 'totalstoriespermonth':
+          $data = $estimates->gettotalStoriesPerMonth();
+          break;
+        default:
+          throw new \Exception("I don't know what ". $type ."is.", 1);
+          
       }
+      
 
       $response = new JsonResponse();
-      $response->setData(array('cols' => $columns, 'rows' => $rows));
+      $response->setData($data);
       $response->headers->set('Content-Type', 'application/json');
       return $response;
     }
-
 }
--- src/Application/DefaultBundle/Lib/Estimates.php
+++ src/Application/DefaultBundle/Lib/Estimates.php
@@ -3,6 +3,20 @@
 namespace Application\DefaultBundle\Lib;
 
 class Estimates {
+
+    /**
+     * The easybacklog client
+     **/
+    private $easybacklogClient;
+
+    /**
+     * Set the stuff
+     * @param $easybacklogClient obj 
+     **/
+    public function __construct(\MikePearce\EasybacklogApiBundle\Client\Client $easybacklogClient) {
+        $this->easybacklogClient = $easybacklogClient;
+    }
+
     /**
      * Return all the aggregated estimate data.
      * @param $stories array - All the stories
@@ -38,6 +52,7 @@ class Estimates {
         $new = $aggEstimates;
         $aggEstimates = array();
         foreach($new AS $year => $month) {
+            ksort($month);
             foreach($month AS $monthNo => $estimates) {
                 ksort($estimates);
                 $aggEstimates[$year][$monthNo] = $estimates;
@@ -45,5 +60,91 @@ class Estimates {
         }
 
         return $aggEstimates;
+    }    
+
+        /**
+     * Get the total of each sized estimate per month
+     * @param $estimates Instance of Estimates
+     * @param $easybacklogClient Instance of \MikePearce\EasybacklogApiBundle\Client\Client
+     * @return array
+     **/
+    public function gettotalStoriesPerMonth() {
+
+        $columns = array();
+        foreach(array(
+            'Year/Month'     => 'string', 
+            'No. of Stories'   => 'number', 
+            ) AS $field => $type) {
+                $columns[] = array('id' => '', 'label' => $field, 'pattern' => '', 'type' => $type);
+        }
+
+        $estimate_data = $this->getEstimateDataByMonth(
+          $this->easybacklogClient->getStoriesFromTheme()
+        );
+
+        $row = $rows = $row_data = $row_label = array();
+
+        foreach($estimate_data AS $year => $month) {
+            foreach($month AS $month_no => $stories) {
+                $row_label = array(array('v' => $year ."/". $month_no, 'f' => null));
+                foreach($stories AS $story) {
+                    $counter = (!isset($counter) ? 0 : $counter);
+                    $counter += $story;
+                }
+                $row_data = array(array('v' => $counter , 'f' => null));
+                $row['c'] = array_merge($row_label, $row_data);
+                $rows[] = $row;
+                $counter = 0;
+                $row = $row_data = $row_label = array();
+            }
+        }
+        return array('cols' => $columns, 'rows' => $rows);
+    }
+
+    /**
+     * Get the total of each sized estimate per month
+     * @param $easybacklogClient Instance of \MikePearce\EasybacklogApiBundle\Client\Client
+     * @return array
+     **/
+    public function getEstimateSpreadPerMonth() {
+
+      // Do the columns
+      $columns = array();
+      foreach(array(
+        'Month'     => 'string', 
+        'Size: 1'   => 'number', 
+        'Size: 2'   => 'number', 
+        'Size: 3'   => 'number', 
+        'Size: 5'   => 'number', 
+        'Size: 8'   => 'number', 
+        'Size: 13'  => 'number', 
+        'Size: 20'  => 'number') AS $field => $type) {
+        $columns[] = array('id' => '', 'label' => $field, 'pattern' => '', 'type' => $type);
+      }
+
+      // Do the data
+      $rows = array();
+      
+      
+      $estimate_data = $this->getEstimateDataByMonth(
+          $this->easybacklogClient->getStoriesFromTheme()
+      );
+
+
+      $rows = array();
+      foreach($estimate_data AS $year => $month) {
+        foreach($month AS $month_no => $estimates) {
+          $row_label = array(array('v' => $year ."/". $month_no, 'f' => null));
+          foreach (array(1, 2, 3, 5, 8, 13, 20) AS $est) {
+            $row_data[] = array('v' => (isset($estimates[$est]) ? $estimates[$est] : 0), 'f' => null);
+          }
+          $row['c'] = array_merge($row_label, $row_data);
+          $rows[] = $row;
+          $row = $row_label = $row_data = array();
+        }
+        
+      }
+
+      return array('cols' => $columns, 'rows' => $rows);
     }    
 }
