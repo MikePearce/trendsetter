3a035dfa4cf42e03b03deef2ddfd6b4c5318f07c
Mike Pearce <mike@mikepearce.net>
2013-05-08 12:49:55 +0100

    Added homepage stats

--- src/Application/DefaultBundle/Controller/DefaultController.php
+++ src/Application/DefaultBundle/Controller/DefaultController.php
@@ -3,15 +3,31 @@
 namespace Application\DefaultBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Application\DefaultBundle\Lib\Estimates;
+use Application\DefaultBundle\Lib\Stories;
+use Application\DefaultBundle\Lib\Velocity;
 
 class DefaultController extends Controller
 {    
     public function indexAction()
     {
+        $teams = $this->container->getParameter('teams');
+
+        $backlogs = array();
+        foreach($teams AS $team) {
+            $backlogs[] = $team['backlog'];
+        }
+        $easybacklogClient = $this->get('mikepearce_easybacklog_api');
+        $easybacklogClient->setAccountId('477')
+                          ->setBacklog($backlogs);
+
+        $vel = new Velocity($easybacklogClient, $this->get('memcached'));
+        $str = new Stories($easybacklogClient, $this->get('memcached'));
         return $this->render(
             'ApplicationDefaultBundle:Default:index.html.twig', 
-            array('stuff' => 'stuff')
+            array(
+                'current_velocity'      => $vel->getCurrentVelocity(),
+                'current_acceptance'    => $str->getCurrentAcceptanceRate(),
+            )
         );
     }
 
