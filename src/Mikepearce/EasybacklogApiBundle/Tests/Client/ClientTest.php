b507d9095f28d0461e0c020bbc982a92d70c80e9
Mike Pearce <mike@mikepearce.net>
2013-05-11 10:36:00 +0100

    Added tests for setters and getting json from API

---
+++ src/Mikepearce/EasybacklogApiBundle/Tests/Client/ClientTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Mikepearce\EasybacklogApiBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Mikepearce\EasybacklogApiBundle\Client\Client;
+
+class ClientClientTest extends \Guzzle\Tests\GuzzleTestCase
+{
+    /**
+     * What we're testing
+     **/
+    public $ebclient;
+    
+    /**
+     * Create the mock clases
+     **/
+    public function setup() {
+
+        // Mock memcache
+        $memcache = $this->getMock('memcache', array('get', 'set'));
+        $memcache->expects($this->any())
+                 ->method('get')
+                 ->will($this->returnValue('{"json":"rocks"}'));
+        $memcache->expects($this->any())
+                 ->method('set')
+                 ->will($this->returnValue(true));        
+
+        // Guzzle
+        $plugin = new \Guzzle\Plugin\Mock\MockPlugin();
+        $plugin->addResponse(new \Guzzle\Http\Message\Response(200));
+        $mockedClient = new \Guzzle\Service\Client();
+        $mockedClient->addSubscriber($plugin);
+        $this->setMockBasePath(__DIR__ . DIRECTORY_SEPARATOR . '../TestData');
+        $this->setMockResponse($mockedClient, 'json_response');
+
+        $this->ebclient = new Client(
+            $memcache, 
+            $mockedClient, 
+            'xxxxxxxxxx', 
+            '123'
+        );
+    }
+
+    /**
+     * @covers Mikepearce\EasybacklogApiBundle\Client\Client\setBacklog
+     * @covers Mikepearce\EasybacklogApiBundle\Client\Client\setAccountId
+     **/
+    public function testSetBacklogReturnsObject()
+    {   
+        
+        $ebclient = $this->ebclient->setBacklog(array('0'))->setBacklog('123');
+        $this->assertInstanceOf('Mikepearce\EasybacklogApiBundle\Client\Client', $ebclient);
+    }
+
+    /**
+     * @covers Mikepearce\EasybacklogApiBundle\Client\Client\getJsonFromApi
+     **/
+    public function testGetJsonFromApiReturnsJson() {
+
+        $json = $this->ebclient->getJsonFromApi('http://www.google.com');
+        $this->assertNotNull(json_decode($json));
+        
+    }
+}
