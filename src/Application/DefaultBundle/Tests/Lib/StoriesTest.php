45882736e16061797bcfd86196b6dc790a933777
Mike Pearce <mike@mikepearce.net>
2013-05-16 12:33:40 +0100

    Added a SetupTests base class to stop copypasta

--- src/Application/DefaultBundle/Tests/Lib/StoriesTest.php
+++ src/Application/DefaultBundle/Tests/Lib/StoriesTest.php
@@ -6,5 +6,12 @@ use Application\DefaultBundle\Tests\SetupTests;
 class StoriesTest extends SetupTests
 {
     public $stories;
-    public $ebclient;   
+    public $ebclient;
+    
+    public function testGetMonthlyAcceptance() {
+        $this->getMocks('json_response_sprints');
+        $ebclient = $this->ebclient->setBacklog(array('0'));
+        $testable = $this->stories->getMonthlyAcceptance($ebclient->getSprints());
+        $this->assertTrue(is_int($testable));
+    }
 }
